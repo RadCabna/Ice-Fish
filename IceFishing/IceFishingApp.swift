1180cc6291bd8a65d6e973b5aa7eeeb76165f14b
Radcabna <radcabna@gmail.com>
2026-05-25 19:33:08 +0300

    Initial Commit

---
+++ IceFishing/IceFishingApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceFishingApp.swift
+//  IceFishing
+//
+//  Created by Алкександр Степанов on 25.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct IceFishingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
